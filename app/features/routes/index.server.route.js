fa59392ff5358d60e1080620b46b5cc17d77b2b1
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-21 15:42:11 +0100

    Add sign out feature and clean up code

--- app/features/routes/index.server.route.js
+++ app/features/routes/index.server.route.js
@@ -1,10 +1,10 @@
 var controllerData = require('../controllers/controller');
 module.exports = function(app) {
-  app.route('/users/signup').post(controllerData.warriorSignup);
-  app.route('/users/login').post(controllerData.warriorLogin);
-  // app.route('/users/:username').get(controllerData.getUserByName);
-  // app.route('/users/signout').post(controllerData.Usersignout);
+  app.route('/users/signup').post(controllerData.userSignup);
+  app.route('/users/login').post(controllerData.userLogin);
+  //app.route('/users/:username').get(controllerData.getUserByName);
+  app.route('/users/signout').post(controllerData.userSignout);
   // app.route('/users/edit').put(controllerData.updateOneUser);
-  // app.route('/users/delete').delete(controllerData.UserDelete);
+  app.route('/users/delete').delete(controllerData.userDelete);
   // app.route('users/delete').delete(controllerData.UserDelete);
 };  
