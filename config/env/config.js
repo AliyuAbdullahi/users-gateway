da3294a2d23b390fde356bc818ba02f77e21d097
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-23 15:37:29 +0100

    modify the config file for production

--- config/env/config.js
+++ config/env/config.js
@@ -1,5 +1,17 @@
 module.exports=function(){
   return{
-     port: process.env.PORT || 5000
+     port: process.env.PORT || 5000,
+     development:{
+                  url:{
+                    userService:"http://localhost:4000",
+                    badgeService:"http://localhost:5555"
+                  }
+     },
+     production:{
+                  url:{
+                    userService:"",
+                    badgeService:""
+                  }
+     }
   }
 }
