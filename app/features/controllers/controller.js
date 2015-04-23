456ad13df33617b6203b9f217d74476c5af76589
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-23 23:44:49 +0100

    modify the authentication

--- app/features/controllers/controller.js
+++ app/features/controllers/controller.js
@@ -2,212 +2,210 @@ var request = require('request');
 var jwt = require('jsonwebtoken');
 var config = require('../../../config/env/config')();
 module.exports = {
-    /* User initialised */
+  /* User initialised */
 userSignup: function(req, res, body) {
-  request.post({
-    url:config.development.url.userService+"/users/signup",
+    request.post({
+      url: config.development.url.userService + "/users/signup",
+         form: req.body
+      },
+      function(err, httpResponse, body) {
+         if (err) {
+         console.log(err);
+        }
+        else {
+          console.log(body);
+          var data = JSON.parse(body);
+          res.json(data);
+        }
+      });
+  },
+  /* User login */
+userLogin: function(req, res) {
+    request.post({
+      url: config.development.url.userService + "/users/login",
       form: req.body
-    },
+    }, function(err, httpResponse, body) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(body);
+      }
+    });
+  },
+  /* User signout */
+userSignout: function(req, res) {
+    request.post({
+      url: config.development.url.userService + "/users/signout",
+      form: req.body
+    }, function(err, httpResponse, body) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(body);
+      }
+    });
+  },
+  /*Obtain one user*/
+getOneUser: function(req, res) {
+    request.post({
+      url: config.development.url.userService + "/users/username",
+      form: {
+      username: req.body.username
+      }
+    }, function(err, httpResponse, body) {
+      if (err) {
+        console.log(err);
+      } else {
+        var data = JSON.parse(body);
+        res.json(data);
+      }
+    });
+  },
+  /*Get all users*/
+getAllUsers: function(req, res) {
+    request.get({
+        url: config.development.url.userService + "/users/signup"
+      },
       function(err, httpResponse, body) {
         if (err) {
           console.log(err);
-        } 
-        else {
-              console.log(body);
-              var data = JSON.parse(body);
-              res.json(data);
-              }
-            });
-          },
-    /* User login */
-userLogin: function(req, res) {
-  request.post({
-  url: config.development.url.userService+"/users/login",
-      form: req.body
-      }, function(err, httpResponse, body) {
-        if (err) {
-          console.log(err);
-        } 
-        else {
-              res.json(body);
-              }
-           });
-         },
-    /* User signout */
-userSignout: function(req, res) {
-request.post({
-  url: config.development.url.userService+"/users/signout",
-      form: req.body
-      }, function(err, httpResponse, body) {
-        if (err) {
-          console.log(err);
-        } 
-        else {
-              res.json(body);
-              }
-           });
-  // request.post({
-  // url: config.development.url.userService+"users/verify",
-  //     form: {
-  //     auth: req.body.auth
-  //     }
-  // }, function(error, httpResponse, body) {
-  //     if (error) {
-  //       res.json(error);
-  //       } 
-  //       else {
-  //           request.post({
-  //           url: config.development.url.userService+"/users/signout",
-  //           form: {
-  //           username: body.username
-  //           }
-  //         }, function(err, httpResponse, body) {
-  //           if (err) {
-  //           console.log(err);
-  //           } 
-  //           else {
-  //           var data = JSON.parse(body);
-  //           res.json(data);
-  //           }
-  //         });
-  //       }
-  //     });
-    },
-getOneUser:function(req, res) {
-  request.post({
-    url: config.development.url.userService+"/users/username",
-    form:{
-      username: req.body.username
-    }
-  },function(err, httpResponse, body) {
-      if (err) {
-        console.log(err);
-        }   else {
-            var data = JSON.parse(body);
+        } else {
+          var data = JSON.parse(body);
           res.json(data);
-          }
-        });
-      },
-getAllUsers: function(req, res) {
-  request.get({
-    url: config.development.url.userService+"/users/signup"
+        }
+      });
   },
-     function(err, httpResponse, body) {
-      if (err) {
-        console.log(err);
-        }   else {
-            var data = JSON.parse(body);
-          res.json(data);
-          }
-        });
+  /*Update user*/
+setuser: function(req, res) {
+    request.post({
+        url: config.development.url.userService + "/users/verify",
+        form: {
+          auth: req.body.auth
+        }
       },
-
-modifyUserDetails: function(req, res) {
-  request.post({
-    url: config.development.url.userService+"/users/verify",
-    form: {
-      auth: req.body.auth
-    }
-    }, function(error, httpResponse, body) {
+      function(error, httpResponse, body) {
         if (error) {
           res.json(error);
-        } 
-        else {
-        req.body.oldname = body.username;
-        request.put({
-        url: config.development.url.userService+"/users/edit",
-        form: {"req.body.oldname":body.username}
-        }, function(err, httpResponse, body) {
-          if (err) {
+        } else {
+          request.put({
+            url: config.development.url.userService + "/users/edit",
+            form: {
+              oldname: req.body.oldname,
+              username: req.body.username
+            }
+          }, function(err, httpResponse, body) {
+            if (err) {
               console.log(err);
-          } 
-            else {
-            var data = JSON.parse(body);
-            if (body.error) {
-            res.json({
-              error: "Sorry data doesn't match "
-            });
-            } 
-              else {
+            } else {
+              var data = JSON.parse(body);
+                if (body.error) {
                 res.json({
-                Success: "user detail updated"
+                  error: "Sorry data doesn't match "
+                });
+              } else {
+                res.json({
+                  Success: "user detail updated"
                 });
               }
             }
           });
         }
       });
-    },
-  /* delete user*/
-userDelete: function(req, res) {
-  request.post({
-    url: config.development.url.userService+'/users/verify',
+  },
+/*dummie function*/
+modifyUserDetails: function(req, res) {
+    request.post({
+      url: config.development.url.userService + "/users/verify",
       form: {
       auth: req.body.auth
       }
-  }, function(error, httpResponse, body) {
+    }, function(error, httpResponse, body) {
       if (error) {
         res.json(error);
       } else {
-        request.del({
-        url: config.development.url.userService+'/users/delete',
-        form: {
-          username: body.username
-        }
-      }, function(err, httpResponse, body) {
-         if (err) {
+        req.body.oldname = body.username;
+        request.put({
+          url: config.development.url.userService + "/users/edit",
+          form: {
+            "req.body.oldname": body.username
+          }
+        }, function(err, httpResponse, body) {
+          if (err) {
             console.log(err);
-         } else {
-                request.del({
-                url: config.development.url.badgeService+'/users/delete',
-                form: {
-                badge: req.body.badge_title,
-                badge_type: req.body.badge_type,
-                total: req.body.total
-                  }
-                }, function(err, httpResponse, body) {
-                      if (err) {
-                        console.log(err);
-                      } else {
-                        res.json({
-                          Success: "User badge and account deleted"
-                        });
-                      }
-                    });
-                  }
-                });
-              }
-            });
-          },
+          } else {
+            var data = JSON.parse(body);
+            if (body.error) {
+              res.json({
+                error: "Sorry data doesn't match "
+              });
+            } else {
+              res.json({
+                Success: "user detail updated"
+              });
+            }
+          }
+        });
+      }
+    });
+  },
+/*Delete User*/
+userDelete: function(req, res) {
+    request.post({
+        url: config.development.url.userService + "/users/verify",
+        form: {
+          auth: req.body.auth
+        }
+      },
+      function(error, httpResponse, body) {
+        if (error) {
+          res.json(error);
+        } else {
 
-          //creating a new badge
+          request.del({
+              url: config.development.url.userService + "/users/delete",
+              form: {
+                username: req.body.username
+              }
+            },
+            function(err, httpResponse, body) {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log(body);
+                res.json(body);
+              }
+          });
+         }
+      });
+     },
+  //creating a new badge
 createBadgeForUser: function(req, res) {
-  request.post({
-    url: config.development.url.userService+'/users/verify',
-    form: {
-      auth: req.body.auth
-    }
-  }, function(error, httpResponse, body) {
+    request.post({
+      url: config.development.url.userService + "/users/verify",
+      form: {
+        auth: req.body.auth
+      }
+    }, function(error, httpResponse, body) {
       if (error) {
         res.json(error);
       } else {
-              console.log(body);
-              request.post({
-              url: config.development.url.badgeService+'/users/',
-              form: {
-              badge_title: req.body.badge_title,
-              badge_type: req.body.badge_type,
-              total: req.body.total
-                }
-              }, function(err, httpResponse, body) {
-                  if (err) {
-                    console.log(err);
-                  } else {
-                    var data = JSON.parse(body);
-                    res.json(data);
-                  }
-                });
-              }
-            });
-          },
-        };
+        console.log(body);
+        request.post({
+          url: config.development.url.badgeService + '/users/',
+          form: {
+            badge_title: req.body.badge_title,
+            badge_type: req.body.badge_type,
+            total: req.body.total
+          }
+        }, function(err, httpResponse, body) {
+          if (err) {
+            console.log(err);
+          } else {
+            var data = JSON.parse(body);
+            res.json(data);
+          }
+        });
+      }
+    });
+  },
+};
