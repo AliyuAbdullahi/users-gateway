f0c078f8e17e9d099516050879e6cf9f2d25b78d
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-23 15:38:04 +0100

    Implement the change to the server to prepare the server for production

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var config = require('./config/config')();
+var config = require('./config/env/config')();
 var app = require('./config/express')();
-app.listen(4000);
-// app.use('/',function (req,res){
-//   res.send("GateWay to user's Destiny");
-// });
+app.listen(config.port, function () {
+    console.log('Express Servicce  app listening on port: ' + config.port);
+});
+app.use('/',function (req,res){
+  res.send("GateWay to user's Destiny");
+});
 module.exports = app;
